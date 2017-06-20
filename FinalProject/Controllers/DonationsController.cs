1605e44ade892ec11eca9fd9769cce8989d60f06
Dylan Wilkinson <dylanwilkinson@gmail.com>
2017-06-20 10:10:37 -0700

    initial commit

---
+++ FinalProject/Controllers/DonationsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FinalProject.Models;
+
+namespace FinalProject.Controllers
+{
+    public class DonationsController : Controller
+    {
+        private Community_AssistEntities1 db = new Community_AssistEntities1();
+
+        // GET: Donations
+        public ActionResult Index()
+        {
+            var donations = db.Donations.Include(d => d.Person);
+            return View(donations.ToList());
+        }
+
+        // GET: Donations/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Donation donation = db.Donations.Find(id);
+            if (donation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donation);
+        }
+
+        // GET: Donations/Create
+        public ActionResult Create()
+        {
+            ViewBag.PersonKey = new SelectList(db.People, "PersonKey", "PersonLastName");
+            return View();
+        }
+
+        // POST: Donations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "DonationKey,PersonKey,DonationDate,DonationAmount,DonationConfirmation")] Donation donation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Donations.Add(donation);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.PersonKey = new SelectList(db.People, "PersonKey", "PersonLastName", donation.PersonKey);
+            return View(donation);
+        }
+
+        // GET: Donations/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Donation donation = db.Donations.Find(id);
+            if (donation == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.PersonKey = new SelectList(db.People, "PersonKey", "PersonLastName", donation.PersonKey);
+            return View(donation);
+        }
+
+        // POST: Donations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "DonationKey,PersonKey,DonationDate,DonationAmount,DonationConfirmation")] Donation donation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(donation).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.PersonKey = new SelectList(db.People, "PersonKey", "PersonLastName", donation.PersonKey);
+            return View(donation);
+        }
+
+        // GET: Donations/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Donation donation = db.Donations.Find(id);
+            if (donation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donation);
+        }
+
+        // POST: Donations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Donation donation = db.Donations.Find(id);
+            db.Donations.Remove(donation);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
